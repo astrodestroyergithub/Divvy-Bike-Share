23678bb55c85aad4ca3074b0bd5f9dcb54c2d092
Tamojit Roy <astrodestroyer146@gmail.com>
2022-11-13 14:16:08 +0530

    Test Commit

--- CODE/script.r
+++ CODE/script.r
@@ -1,7 +1,8 @@
 # update Working directory to this script path
 library(rstudioapi)
 script_dir = rstudioapi::getActiveDocumentContext()$path
-setwd('F:/Programming/OneDrive/Documents/')
+setwd(dirname(script_dir))
+getwd()
 
 # load Packages
 library(readr)
@@ -10,3 +11,25 @@ library(dplyr)
 library(lubridate)
 library(skimr)
 library(janitor)
+
+# load the csv file for analysis
+df <- read_csv("divvy_trips.csv")
+str(df)
+
+# taking a sample from the data for simplicity
+sample_df <- sample_n(df,76000,replace=F)
+
+# writing the sample into a sample dataset
+write_csv(sample_df,"sample_dataset.csv")
+str(sample_df)
+
+# loading the sample dataset
+df <- read_csv("sample_dataset.csv")
+
+# exploring various attributes of sample
+colnames(df)
+nrow(df)  
+dim(df) 
+head(df)  
+str(df)  
+summary(df)
